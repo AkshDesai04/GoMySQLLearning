5592ec56edfe21fd0b2b32b6608bec9c2d0e8ed8
Aksh Desai <aksh.d.4002@gmail.com>
2025-04-29 15:46:29 +0530

    Corrected webpage 404 issue

--- static_server/main.go
+++ static_server/main.go
@@ -3,12 +3,32 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func main() {
-	fs := http.FileServer(http.Dir("../static"))
+	// Get the current working directory
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Fatal("Error getting current working directory:", err)
+	}
+
+	// Construct the path to the static directory
+	staticPath := filepath.Join(cwd, "static")
+
+	// Log the path being used
+	log.Printf("Current working directory: %s", cwd)
+	log.Printf("Serving static files from: %s", staticPath)
+
+	// Check if the directory exists
+	if _, err := os.Stat(staticPath); os.IsNotExist(err) {
+		log.Fatalf("Static directory not found at: %s", staticPath)
+	}
+
+	fs := http.FileServer(http.Dir(staticPath))
 	http.Handle("/", fs)
 
 	log.Println("Static file server starting on :8083")
 	log.Fatal(http.ListenAndServe(":8083", nil))
-} 
+}
